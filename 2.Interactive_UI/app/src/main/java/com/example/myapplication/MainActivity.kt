e9401b8b0500ffa14f4fcfdfbeed54b0d117941e
Manis7733 <69597974+Manis7733@users.noreply.github.com>
2023-03-21 02:24:53 +0530

    Interactive_UI

---
+++ 2.Interactive_UI/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import android.widget.Button
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ButtonDark = findViewById<Button>(R.id.btnDark)
+        val ButtonRead = findViewById<Button>(R.id.btnRead)
+        val Layout = findViewById<LinearLayout>(R.id.LinearLayout)
+
+        ButtonRead.setOnClickListener{
+            Layout.setBackgroundResource(R.color.yellow)
+        }
+        ButtonDark.setOnClickListener {
+            Layout.setBackgroundResource(R.color.black)
+        }
+    }
+}
